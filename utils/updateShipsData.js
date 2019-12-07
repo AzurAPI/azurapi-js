183cc8960624dc7a2fae3ca6be7b80c0bb1a9d46
jasperchua99 <17019686@rp.edu.sg>
2019-12-07 14:40:34 +0800

    Path to Absolute location of the json

--- utils/updateShipsData.js
+++ utils/updateShipsData.js
@@ -4,16 +4,17 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.updateShipsData = exports.default = void 0;
-
 var _nodeFetch = _interopRequireDefault(require("node-fetch"));
 
 var _fs = _interopRequireDefault(require("fs"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const path = require('path');
 
 const updateShipsData = async () => {
+  var publicFolder = path.join(__dirname, '../lib/ships.json')
   await (0, _nodeFetch.default)('https://raw.githubusercontent.com/AzurAPI/azurapi-js-setup/master/ships.json').then(resp => resp.json()).then(responseJSON => {
-    _fs.default.writeFile("./lib/ships.json", JSON.stringify(responseJSON), function (err) {
+    _fs.default.writeFile(publicFolder, JSON.stringify(responseJSON), function (err) {
       if (err) {
         console.log(err);
       }
