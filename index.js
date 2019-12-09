6c1ac960dea0149ee3781917360ba82420cafe36
tommywalkie <tommywalkie@gmail.com>
2019-12-09 18:51:11 +0100

    Testing checkForNewUpdate from Github Actions

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const ships = require('./lib')
 const { updateShipsData } = require('./lib/updateShipsData')
+const { checkForNewUpdate } = require('./lib/checkForNewUpdate')
 
 module.exports = {
   ships,
   ...ships,
   updateShipsData,
+  checkForNewUpdate
 }
