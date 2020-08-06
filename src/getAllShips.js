1820d3f20a1bcecf23ef61e22f72538e9059a729
tommywalkie <tommywalkie@gmail.com>
2020-08-06 19:47:03 +0200

    Fixing ship ID sorting issue

--- src/getAllShips.js
+++ src/getAllShips.js
@@ -1,7 +1,8 @@
-import { toArray } from 'lodash'
+import toArray from 'lodash.toarray'
 import getAllShipsFromJson from './getAllShipsFromJson'
 
 const getAllShips = toArray(getAllShipsFromJson)
+const getAllShips = sortBy(toArray(getAllShipsFromJson), ['id'])
 
 export default getAllShips
 export { getAllShips as getAllShips }
