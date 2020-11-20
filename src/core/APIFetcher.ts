b36f82f1349f44c7c6589e25c543c4908404ca0d
0t4u <chiuyiyi1234@gmail.com>
2020-11-20 20:20:29 +0800

    remove commented

--- src/core/APIFetcher.ts
+++ src/core/APIFetcher.ts
@@ -253,8 +253,6 @@ export default class APIFetcher {
     const data = await this.getDataEquipments();
     const escapeLatinString = (string: any) => string.toLowerCase(string.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[!@#$%^&*(),.?":{}|<>' ]/g, ''));
     let find = Object.keys(data).findIndex(item => escapeLatinString(item).includes(escapeLatinString(id)));
-    //let find = data.indexOf((item: string) => escapeLatinString(item).includes(escapeLatinString(id)));
-    //let find = data.findIndex(item => escapeLatinString(item).includes(escapeLatinString(id)));
     if (data[find]) return (data[find]);
     throw new UnknownEquipmentError(id);
   }
