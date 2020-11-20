b8ea8ea3e7f86099148aff275bbaff99c110a389
0t4u <chiuyiyi1234@gmail.com>
2020-11-20 08:13:02 +0800

    forgot to commit

--- tests/suite.test.js
+++ tests/suite.test.js
@@ -1,7 +1,12 @@
 const { AzurAPIClient } = require('../build/AzurAPIClient');
 const client = new AzurAPIClient();
 
-client
+/*client
   .getShipsByFaction('IJN')
   .then((data) => console.log(data[0]))
+  .catch(console.error);
+console.log('-----------------');*/
+client
+  .getEquipments('Quadruple 130mm (Mle 1932)')
+  .then((data) => console.log(data))
   .catch(console.error);
