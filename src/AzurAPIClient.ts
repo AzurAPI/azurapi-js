cc15e5f04acdb0280aa6dbac6505595e43b8c60c
0t4u <61939142+0t4u@users.noreply.github.com>
2020-12-06 13:17:23 +0800

    implement suggestion | dev-2.0.8.3
    https://discord.com/channels/648206344729526272/648211105625276436/784058108317925386

--- src/AzurAPIClient.ts
+++ src/AzurAPIClient.ts
@@ -14,14 +14,12 @@ import Updater from './core/Updater';
 interface NulledClientOptions {
   directory?: string;
   update?: number | boolean;
-  online?: true | false;
-  customDataLocation?: string;
+  customDataURL?: string;
 }
 
 interface NonNulledClientOptions {
   directory: string;
   update: number | boolean;
-  online: true | false;
   customDataURL: string | undefined;
 }
 
@@ -47,7 +45,7 @@ export class AzurAPIClient extends EventEmitter {
   /**
    * Custom data url
    */
-  private dataURL;
+  private dataURL: any;
 
   /**
    * The options the user has set
@@ -66,12 +64,17 @@ export class AzurAPIClient extends EventEmitter {
     this.options = merge<any, NonNulledClientOptions>(options, {
       directory: join(process.cwd(), '.azurapi'),
       update: false,
-      online: false,
       customDataURL: undefined
     });
     this.updater = new Updater(this.options.directory);
     this.dataURL = this.options.customDataURL ? this.options.customDataURL : false;
 
+    /**
+     * Update if option update is true
+     */
+    if(this.options.update) {
+      this.updater;
+    }
 
     /** 
      * Add backwards compatibility (readonly variable since this is outside of the constructor's scope)
@@ -174,54 +177,114 @@ export class AzurAPIClient extends EventEmitter {
   /**
    * Gets the ship by it's query
    * @param query The query (id or name)
+   * @param online Use online mode (defaults to true)
    * @example <client>.getShip('query');
    */
-  getShip(query: string) {
-    return this.fetcher.getShip(query);
+  getShip(query: string, online?: boolean) {
+    let r;
+    switch(online) {
+      case (false):
+        r = this.local.getShip(query);
+        break;
+      default:
+        r = this.fetcher.getShip(query);
+    }
+    return r;
+    //return this.fetcher.getShip(query);
   }
 
   /**
    * Gets the ship by the faction
    * @param faction The faction to find ships from
+   * @param online Use online mode (defaults to true)
    * @example <client>.getShipByFaction('faction');
    */
-  getShipsByFaction(faction: Nationality) {
-    return this.fetcher.getShipsFromFaction(faction);
+  getShipsByFaction(faction: Nationality, online?: boolean) {
+    let r;
+    switch(online) {
+      case (false):
+        r = this.local.getShipsFromFaction(faction);
+        break;
+      default:
+        r = this.fetcher.getShipsFromFaction(faction);
+    }
+    return r;
+    //return this.fetcher.getShipsFromFaction(faction);
   }
 
   /**
    * Gets the equipment by it's query
    * @param query The query (name)
+   * @param online Use online mode (defaults to true)
    * @example <client>.getEquipment('query');
    */
-  getEquipment(query: string) {
-    return this.fetcher.getEquipment(query/*, type*/);
+  getEquipment(query: string, online?: boolean) {
+    let r;
+    switch(online) {
+      case (false):
+        r = this.local.getEquipment(query);
+        break;
+      default:
+        r = this.fetcher.getEquipment(query);
+    }
+    return r;
+    //return this.fetcher.getEquipment(query/*, type*/);
   }
 
   /**
    * Gets chapter by query
    * @param chapter The chapter to search for
    * @param section (Optional) The section to search for
+   * @param online Use online mode (defaults to true)
    * @example <client>.getChapter('query', 'section');
    */
-  getChapter(chapter: string, section?: string) {
-    return this.fetcher.getChapter(chapter, section);
+  getChapter(chapter: string, section?: string, online?: boolean) {
+    let r;
+    switch(online) {
+      case (false):
+        r = this.local.getChapter(chapter, section);
+        break;
+      default:
+        r = this.fetcher.getChapter(chapter, section);
+    }
+    return r;
+    //return this.fetcher.getChapter(chapter, section);
   }
   
   /**
    * Gets the voice line for ship by it's query
    * @param query The query (ship ID)
+   * @param online Use online mode (defaults to true)
    * @example <client>.getVoiceline('query');
    */
-  getVoiceline(query: string) {
-    return this.fetcher.getVoiceline(query);
+  getVoiceline(query: string, online?: boolean) {
+    let r;
+    switch(online) {
+      case (false):
+        r = this.local.getVoiceline(query);
+        break;
+      default:
+        r = this.fetcher.getVoiceline(query);
+    }
+    return r;
+    //return this.fetcher.getVoiceline(query);
   }
   /**
    * Gets barrage from ID
    * @param query The query (barrage ID)
+   * @param online Use online mode (defaults to true)
    * @example <client>.getBarrage('query');
    */
-  getBarrage(query: string) {
-    return this.fetcher.getBarrage(query);
+  getBarrage(query: string, online?: boolean) {
+    let r;
+    switch(online) {
+      case (false):
+        r = this.local.getBarrage(query);
+        break;
+      default:
+        r = this.fetcher.getBarrage(query);
+    }
+    return r;
+    //return this.fetcher.getBarrage(query);
   }
 }
