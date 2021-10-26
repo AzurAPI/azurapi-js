c1d48e72c778c3afa472baa910cd10405e1bcf55
Tsukugi <tsukugi@protonmail.com>
2021-10-26 22:20:42 +0200

    Remove dead code

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,6 @@ export { ClientTools, UpdaterTemplate } from './types/client';
 export { Events } from './core/events';
 export { createUpdater, UpdaterProps } from './core/tools/updater';
 export { getClientTools } from './core/tools/toolsHandler';
-export { DataSetUtils } from './core/utils/dataset';
-export { DataSet } from './types/utils/dataset';
 export { FetchAPI, useFetchAPI, FetchAPIProps } from './core/utils/api';
 export { SharedAPI, isIdentifiable, hasNames } from './core/api/shared';
 export { Identifiable, WithNames } from './types/identifiable';
