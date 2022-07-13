a7d6d01c252f928033aa8e65d782c69d3fcda2f2
Sp3cker <speker97@protonmail.com>
2022-07-12 21:11:30 -0500

    Add check for undefined query string in ships_api

--- src/core/api/api_ship.ts
+++ src/core/api/api_ship.ts
@@ -24,6 +24,7 @@ export class Ships extends API<Ship> {
    * @param id String of number
    */
   id(id: string): Ship | undefined {
+    this.queryIsString(id);
     for (let item of this.raw)
       if (normalize(item.id.toUpperCase()) === normalize(id.toUpperCase())) return item;
     return undefined;
@@ -35,6 +36,7 @@ export class Ships extends API<Ship> {
    * @param languages Language to search
    */
   name(name: string, languages: Language[] = ['en', 'cn', 'jp', 'kr']): Ship[] | [] {
+    this.queryIsString(name);
     return this.raw.filter((ship) =>
       languages.some(
         (lang) =>
@@ -49,6 +51,7 @@ export class Ships extends API<Ship> {
    * @param hull Hull name
    */
   hull(hull: string): Ship[] | [] {
+    this.queryIsString(hull);
     return this.raw.filter(
       (ship) =>
         (ship.hullType &&
@@ -63,6 +66,7 @@ export class Ships extends API<Ship> {
    * @param nationality Nationality name
    */
   nationality(nationality: string): Ship[] | [] {
+    this.queryIsString(nationality);
     let results: Ship[] = [];
     nationality =
       Object.keys(NATIONS).find((key) => NATIONS[key].includes(nationality.toLowerCase())) ||
@@ -78,6 +82,7 @@ export class Ships extends API<Ship> {
    * @param query Ship name in any language or ship id
    */
   get(query: string): Ship | Ship[] {
+    this.queryIsString(query);
     if (this.client.queryIsShipName(query)) {
       return this.name(query);
     }
@@ -90,6 +95,7 @@ export class Ships extends API<Ship> {
    * @param query basically anyting i guess
    */
   all(query: string): Ship[] | [] {
+    this.queryIsString(query);
     let results: (Ship | undefined)[] = [];
     results.push(this.id(query));
     results.push(...this.name(query).filter((i) => i));
@@ -100,5 +106,14 @@ export class Ships extends API<Ship> {
   }
   getShipIdByName(name: string, language = 'en') {
     return this.name(name)[0].id;
+  }
+  /**
+   * Throws is query is `undefined` or an object or something – anything not a string.
+   */
+  private queryIsString(query: unknown): query is string {
+    if (typeof query === 'string' && query.trim().length !== 0) {
+      return true;
+    }
+    throw new Error('AzurAPI query string must be string');
   }
 }
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,16 +1,5 @@
-const { AzurAPI } = require('../build/index.js');
 const { Ships } = require('../build/core/api/api_ship');
 const testShips = require('./test_ships.json');
-
-test('Ship by Name/ID', async () => {
-  const mockApi = { queryIsShipName: () => true };
-  const client = new Ships(mockApi);
-  client.raw = testShips;
-
-  let result = client.get('Abukuma');
-
-  return expect(result[0].names.en).toBe('Abukuma');
-});
 
 test('Ships by Faction', async () => {
   const client = new Ships();
@@ -19,4 +8,36 @@ test('Ships by Faction', async () => {
   let result = client.nationality('IJN');
 
   return expect(result[0].nationality).toBe('Sakura Empire');
+});
+
+describe('get', () => {
+  const mockApi = { queryIsShipName: () => true };
+
+  it('should throw if you search with an empty string', () => {
+    const client = new Ships(mockApi);
+    try {
+      let result = client.get(' ');
+    } catch (err) {
+      expect(err.message).toBe('AzurAPI query string must be string');
+    }
+  });
+  it('should throw if search with object', () => {
+    const client = new Ships(mockApi);
+
+    try {
+      let result = client.get({ some: 'damn object' });
+    } catch (err) {
+      expect(err.message).toBe('AzurAPI query string must be string');
+    }
+  });
+
+  it('Ship by Name/ID', async () => {
+    const mockApi = { queryIsShipName: () => true };
+    const client = new Ships(mockApi);
+    client.raw = testShips;
+
+    let result = client.get('Abukuma');
+
+    return expect(result[0].names.en).toBe('Abukuma');
+  });
 });
